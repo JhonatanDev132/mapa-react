d4d0c14b52abb691ddf0940db33cc903ca96b528
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-03-25 09:02:48 -0300

    .

--- App.js
+++ App.js
@@ -1,17 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/screen/Home';
+import MapView from 'react-native-maps';
 
 
 
 export default function App() {
+  const regiaoInicialMapa = {
+    latitude: -10,
+    longitude: -55,
+
+    /* Definição do zoom do mapa.
+    Quanto menor, mais próximo o mapa */
+    latitudeDelta: 40,
+    longitudeDelta: 40,
+  };
   return (
     <>
     <StatusBar barStyle="defalt" />
-    <Home/>
+    <View>
+      <MapView 
+      style={styles.mapa} 
+      initialRegion={regiaoInicialMapa}
+      mapType="standard"
+      userInterfaceStyle="dark"
+      maxZoomLevel={15}
+      minZoomLevel={5}
+      />
+    </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  mapa: {
+    height: "100%",
+    width: "100%"
+  }
 });
