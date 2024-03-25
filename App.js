4f71f238c1b43fe1a9b64ff6472ea5d5ff1ecfac
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-03-25 09:20:38 -0300

    .

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 
 
 export default function App() {
   const regiaoInicialMapa = {
+    /* Brasil
     latitude: -10,
-    longitude: -55,
+    longitude: -55, */
+
+    // São Paulo
+    latitude: -23.533773,
+    longitude: -46.65529,
 
     /* Definição do zoom do mapa.
     Quanto menor, mais próximo o mapa */
     latitudeDelta: 40,
     longitudeDelta: 40,
   };
+
+  /* Coordenadas para o Marker que será aplicado ao MapView */
+  const localizacao = {
+    latitude: -33.867886,
+    longitude: -63.987,
+    latitudeDelta: 10,
+    longitudeDelta: 10,
+  }
   return (
     <>
     <StatusBar barStyle="defalt" />
@@ -23,9 +36,11 @@ export default function App() {
       initialRegion={regiaoInicialMapa}
       mapType="standard"
       userInterfaceStyle="dark"
-      maxZoomLevel={15}
-      minZoomLevel={5}
-      />
+      /* maxZoomLevel={15}
+      minZoomLevel={5} */
+      >
+        <Marker coordinate={localizacao} draggable/>
+      </MapView>
     </View>
     </>
   );
